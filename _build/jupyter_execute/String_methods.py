4bb3b411bd149d7ffb0d7e8517ae53c227bea4f3
laserchemist <jonathan.m.smith@temple.edu>
2022-06-15 18:44:02 +0200

    update
    add html

---
+++ _build/jupyter_execute/String_methods.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### String Methods
+# 
+# #### Examples:
+# |Method name|Value|
+# |-|-|
+# |`replace`|replace part of a string with another string|
+# |`lower`|a lowercased version of the string|
+# |`upper`|an uppercased version of the string|
+# |`capitalize`|a version with the first letter capitalized|
+# |`title`|a version with the first letter of every word capitalized||
+# 
+# 
+
+# In[1]:
+
+
+'Jonathan'.lower()
+
+
+# In[2]:
+
+
+'Temple is good'.replace('good','great')
+
+
+# In[3]:
+
+
+temple = 'Temple is ok'
+temple.replace('ok','great')
+
+
+# In[4]:
+
+
+'Temple'+' is ok'
+
+
+# In[5]:
+
+
+'10' +' 2'
+
+
+# In[6]:
+
+
+number = 1884 # Temple founding date
+
+
+# In[7]:
+
+
+number += 150 # A way to add to value in variable number
+
+
+# In[ ]:
+
+
+
+
